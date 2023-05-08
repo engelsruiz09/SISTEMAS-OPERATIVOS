983dae7f63306d686c21a65fa58e70cf10cc54be
engelsruiz09 <jaruizc@correo.url.edu.gt>
2023-05-07 18:27:41 -0600

    full

---
+++ SEMANA 11/PRACTICA3_SEMAFOROS/Form2.Designer.cs
@@ -0,0 +1,290 @@
+﻿namespace PRACTICA3_SEMAFOROS
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form2));
+            button1 = new Button();
+            button2 = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            lblEntrada = new Label();
+            lblSalida = new Label();
+            pictureBox1 = new PictureBox();
+            txtCantidadVehiculos = new TextBox();
+            txtTiempoSemaforo = new TextBox();
+            label5 = new Label();
+            label6 = new Label();
+            button3 = new Button();
+            button4 = new Button();
+            listBoxcolaentrada = new ListBox();
+            listBoxcolasalida = new ListBox();
+            cmbUnidadTiempo = new ComboBox();
+            label3 = new Label();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.Aquamarine;
+            button1.Font = new Font("Segoe UI", 13.8F, FontStyle.Regular, GraphicsUnit.Point);
+            button1.Location = new Point(200, 202);
+            button1.Name = "button1";
+            button1.Size = new Size(110, 43);
+            button1.TabIndex = 0;
+            button1.Text = "Agregar ";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.Crimson;
+            button2.Font = new Font("Segoe UI", 13.8F, FontStyle.Regular, GraphicsUnit.Point);
+            button2.Location = new Point(781, 206);
+            button2.Name = "button2";
+            button2.Size = new Size(115, 38);
+            button2.TabIndex = 1;
+            button2.Text = "Salida";
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += button2_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = Color.Orange;
+            label1.Font = new Font("Segoe UI", 13.8F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.Location = new Point(12, 205);
+            label1.Name = "label1";
+            label1.Size = new Size(182, 31);
+            label1.TabIndex = 2;
+            label1.Text = "Agregar Entrada";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.BackColor = Color.Orange;
+            label2.Font = new Font("Segoe UI", 13.8F, FontStyle.Regular, GraphicsUnit.Point);
+            label2.Location = new Point(600, 210);
+            label2.Name = "label2";
+            label2.Size = new Size(165, 31);
+            label2.TabIndex = 3;
+            label2.Text = "Agregar Salida";
+            // 
+            // lblEntrada
+            // 
+            lblEntrada.AutoSize = true;
+            lblEntrada.BackColor = Color.Orange;
+            lblEntrada.Font = new Font("Segoe UI", 13.8F, FontStyle.Regular, GraphicsUnit.Point);
+            lblEntrada.Location = new Point(80, 301);
+            lblEntrada.Name = "lblEntrada";
+            lblEntrada.Size = new Size(191, 31);
+            lblEntrada.TabIndex = 4;
+            lblEntrada.Text = "Coches Entrando:";
+            // 
+            // lblSalida
+            // 
+            lblSalida.AutoSize = true;
+            lblSalida.BackColor = Color.Orange;
+            lblSalida.Font = new Font("Segoe UI", 13.8F, FontStyle.Regular, GraphicsUnit.Point);
+            lblSalida.Location = new Point(611, 302);
+            lblSalida.Name = "lblSalida";
+            lblSalida.Size = new Size(186, 31);
+            lblSalida.TabIndex = 5;
+            lblSalida.Text = "Coches Saliendo:";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Location = new Point(404, 77);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(122, 132);
+            pictureBox1.TabIndex = 6;
+            pictureBox1.TabStop = false;
+            // 
+            // txtCantidadVehiculos
+            // 
+            txtCantidadVehiculos.BackColor = SystemColors.MenuText;
+            txtCantidadVehiculos.Font = new Font("Segoe UI", 13.8F, FontStyle.Regular, GraphicsUnit.Point);
+            txtCantidadVehiculos.ForeColor = SystemColors.Menu;
+            txtCantidadVehiculos.Location = new Point(80, 109);
+            txtCantidadVehiculos.Name = "txtCantidadVehiculos";
+            txtCantidadVehiculos.Size = new Size(125, 38);
+            txtCantidadVehiculos.TabIndex = 7;
+            // 
+            // txtTiempoSemaforo
+            // 
+            txtTiempoSemaforo.BackColor = SystemColors.InfoText;
+            txtTiempoSemaforo.Font = new Font("Segoe UI", 13.8F, FontStyle.Regular, GraphicsUnit.Point);
+            txtTiempoSemaforo.ForeColor = SystemColors.Menu;
+            txtTiempoSemaforo.Location = new Point(736, 110);
+            txtTiempoSemaforo.Name = "txtTiempoSemaforo";
+            txtTiempoSemaforo.Size = new Size(125, 38);
+            txtTiempoSemaforo.TabIndex = 8;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.BackColor = Color.Orange;
+            label5.Font = new Font("Segoe UI", 13.8F, FontStyle.Regular, GraphicsUnit.Point);
+            label5.Location = new Point(22, 75);
+            label5.Name = "label5";
+            label5.Size = new Size(242, 31);
+            label5.TabIndex = 9;
+            label5.Text = "Cantidad de Vehiculos";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.BackColor = Color.Orange;
+            label6.Font = new Font("Segoe UI", 13.8F, FontStyle.Regular, GraphicsUnit.Point);
+            label6.Location = new Point(686, 76);
+            label6.Name = "label6";
+            label6.Size = new Size(230, 31);
+            label6.TabIndex = 10;
+            label6.Text = "Tiempo del semaforo";
+            // 
+            // button3
+            // 
+            button3.BackColor = SystemColors.ActiveCaptionText;
+            button3.Location = new Point(435, 204);
+            button3.Name = "button3";
+            button3.Size = new Size(56, 237);
+            button3.TabIndex = 11;
+            button3.UseVisualStyleBackColor = false;
+            // 
+            // button4
+            // 
+            button4.BackColor = SystemColors.ActiveCaptionText;
+            button4.Location = new Point(387, 61);
+            button4.Name = "button4";
+            button4.Size = new Size(154, 162);
+            button4.TabIndex = 12;
+            button4.UseVisualStyleBackColor = false;
+            // 
+            // listBoxcolaentrada
+            // 
+            listBoxcolaentrada.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left;
+            listBoxcolaentrada.BackColor = SystemColors.InfoText;
+            listBoxcolaentrada.Font = new Font("Segoe UI", 10.8F, FontStyle.Regular, GraphicsUnit.Point);
+            listBoxcolaentrada.ForeColor = SystemColors.Menu;
+            listBoxcolaentrada.FormattingEnabled = true;
+            listBoxcolaentrada.ItemHeight = 25;
+            listBoxcolaentrada.Location = new Point(80, 355);
+            listBoxcolaentrada.Name = "listBoxcolaentrada";
+            listBoxcolaentrada.Size = new Size(191, 104);
+            listBoxcolaentrada.TabIndex = 13;
+            // 
+            // listBoxcolasalida
+            // 
+            listBoxcolasalida.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left;
+            listBoxcolasalida.BackColor = SystemColors.InfoText;
+            listBoxcolasalida.Font = new Font("Segoe UI", 10.8F, FontStyle.Regular, GraphicsUnit.Point);
+            listBoxcolasalida.ForeColor = SystemColors.Menu;
+            listBoxcolasalida.FormattingEnabled = true;
+            listBoxcolasalida.ItemHeight = 25;
+            listBoxcolasalida.Location = new Point(611, 356);
+            listBoxcolasalida.Name = "listBoxcolasalida";
+            listBoxcolasalida.Size = new Size(186, 104);
+            listBoxcolasalida.TabIndex = 14;
+            // 
+            // cmbUnidadTiempo
+            // 
+            cmbUnidadTiempo.BackColor = SystemColors.InfoText;
+            cmbUnidadTiempo.Font = new Font("Segoe UI", 13.8F, FontStyle.Regular, GraphicsUnit.Point);
+            cmbUnidadTiempo.ForeColor = SystemColors.Menu;
+            cmbUnidadTiempo.FormattingEnabled = true;
+            cmbUnidadTiempo.Items.AddRange(new object[] { "Segundos", "Minutos" });
+            cmbUnidadTiempo.Location = new Point(781, 154);
+            cmbUnidadTiempo.Name = "cmbUnidadTiempo";
+            cmbUnidadTiempo.Size = new Size(151, 39);
+            cmbUnidadTiempo.TabIndex = 15;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.BackColor = Color.Orange;
+            label3.Font = new Font("Segoe UI", 13.8F, FontStyle.Regular, GraphicsUnit.Point);
+            label3.Location = new Point(551, 157);
+            label3.Name = "label3";
+            label3.Size = new Size(229, 31);
+            label3.TabIndex = 16;
+            label3.Text = "Dimensional Tiempo:";
+            // 
+            // Form2
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
+            BackgroundImageLayout = ImageLayout.Stretch;
+            ClientSize = new Size(941, 504);
+            Controls.Add(label3);
+            Controls.Add(cmbUnidadTiempo);
+            Controls.Add(listBoxcolasalida);
+            Controls.Add(listBoxcolaentrada);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(txtTiempoSemaforo);
+            Controls.Add(txtCantidadVehiculos);
+            Controls.Add(pictureBox1);
+            Controls.Add(lblSalida);
+            Controls.Add(lblEntrada);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "Form2";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "PRACTICA 3 - SEMAFOROS - JULIO RUIZ - 1284719";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private Button button2;
+        private Label label1;
+        private Label label2;
+        private Label lblEntrada;
+        private Label lblSalida;
+        private PictureBox pictureBox1;
+        private TextBox txtCantidadVehiculos;
+        private TextBox txtTiempoSemaforo;
+        private Label label5;
+        private Label label6;
+        private Button button3;
+        private Button button4;
+        private ListBox listBoxcolaentrada;
+        private ListBox listBoxcolasalida;
+        private ComboBox cmbUnidadTiempo;
+        private Label label3;
+    }
+}
---
+++ SEMANA 11/PRACTICA3_SEMAFOROS/Form2.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace PRACTICA3_SEMAFOROS
+{
+    public partial class Form2 : Form
+    {
+
+        public Form2()
+        {
+            InitializeComponent();
+        }
+        private Queue<string> entrada = new Queue<string>(); //se declara la cola de entrada para los vehiculos
+        private Queue<string> salida = new Queue<string>(); // se declara la cola de salida para los vehiculos
+        private SemaphoreSlim semaforo = new SemaphoreSlim(1, 1);//clase liviana en el espacio de nombres System.Threading de .NET que representa un contador de señales que puede utilizarse para controlar el acceso a un recurso compartido o para limitar el número de tareas que pueden ejecutarse en paralelo. Controla el acceso a los cambios de estado del semáforo (verde y rojo) en la simulación de entrada y salida de vehículos.
+        private CancellationTokenSource cancellationTokenSource;//declaro un campo llamado CancellationTokenSource de tipo cancellationTokenSource, esta ultima es una clase en el espacio de nombres System.Threading de .NET esta genera tokens de cancelacion y controlar la cancelacion de operaciones asincronas.
+
+        private async void button1_Click(object sender, EventArgs e) //boton de agregar vehiculos a la cola de entrada
+        {
+            listBoxcolaentrada.Items.Clear();  //para limpiar el listbox cuando ingrese otra cantidad de vehiculos
+            int vehiculos = int.Parse(txtCantidadVehiculos.Text);
+
+            for (int i = 0; i < vehiculos; i++)
+            {
+
+                entrada.Enqueue($"Coche Entrando: {entrada.Count + 1}");//enqueue es un metodo para agregar a la cola de c#
+                lblEntrada.Text = $"Coches Entrando: {entrada.Count}"; //agrega al label la cantidad de vehiculos entrando
+
+                listBoxcolaentrada.Items.Add($"Coche Entrando {entrada.Count}");//agrega el vehiculo al listbox
+            }
+
+            await ControllerSemaphore();// indica que el programa debe esperar a que la función ControllerSemaphore() se complete antes de continuar con la siguiente línea de código en el método que contiene esta llamada, sin bloquear el hilo principal y permitiendo que otras tareas continúen ejecutándose.
+        }
+
+        private async void button2_Click(object sender, EventArgs e)//boton para agregar a la cola de salida los vehiculos
+        {
+            listBoxcolasalida.Items.Clear(); //para limpiar el listbox cuando ingrese otra cantidad de vehiculos
+            int vehiculos = int.Parse(txtCantidadVehiculos.Text);
+
+            for (int i = 0; i < vehiculos; i++)
+            {
+                salida.Enqueue($"Coche Saliendo: {salida.Count + 1}");//enqueue es un metodo para agregar a la cola de c#
+                lblSalida.Text = $"Coches Saliendo: {salida.Count}";//agrega al label la cantidad de vehiculos saliendo
+
+                listBoxcolasalida.Items.Add($"Coche Saliendo {salida.Count}"); //agrega el vehiculo al listbox
+            }
+
+            await ControllerSemaphore(); //para ser uso del metodo de controlsemaforo se tiene que hacer el metodo que lo contiene async
+        }
+
+        //defino un metodo privado y asincrono que no devuelve un resultado solo una tarea que indica la finalizacion de la operacion en segundo plano
+        //async indica que puede realizar operaciones asincronas sin bloquear el hilo principal de la ejecucion.
+
+        //system.Threading.Task.Task Task indica que el metodo devuelve una tarea que representa una operacion en segundo plano, permite el uso de await en operaciones que demoran algun tiempo en completarse.
+        private async System.Threading.Tasks.Task ControllerSemaphore()
+        {
+            //compruebo si mi variable cancellationTokenSource no es null, si no es null quiere decir que existe un cancellationTokenSource ya creado y su token podria haber sido utilizado para iniciar operaciones asincronas.
+            if (cancellationTokenSource != null)
+            {
+                //si no es null llamo al metodo cancel() de cancellationTokenSource le manda una señal de cancelacion al token de cancelacion asociada a ese cancellationTokenSource
+                //las operaciones asincronas que utilizan este token verifican si se ha solicitado la cancelacion y de ser asi se cancelaran.
+                cancellationTokenSource.Cancel();
+            }
+
+            cancellationTokenSource = new CancellationTokenSource();//creo una nueva instancia y se asigna en cancellationTokenSource aqui se genera un nuevo cancellationTokenSource y su token de cancelacion asociado que se utilizara para la siguiente operacion asincrona 
+            var token = cancellationTokenSource.Token;//se obtiene el token de cancelacion nuevo cancellationTokenSource utilizando .Token y lo guardo en la variable token.
+
+            if (string.IsNullOrEmpty(txtTiempoSemaforo.Text))
+            {
+                MessageBox.Show("Por favor, ingrese un valor en el campo 'Tiempo de semáforo'", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                listBoxcolaentrada.Items.Clear();//si no ingreso el tiempo el listbox ingresa la cantidad de vehiculos con esta linea hago que se borre la informacion del listbox para volver a ingresar la cantidad de vehiculos
+                entrada.Clear();//reiniciamos el contador de los vehiculos
+
+                return;
+            }
+
+            int tiempoSemaforo = int.Parse(txtTiempoSemaforo.Text);
+
+            string unidadTiempo = cmbUnidadTiempo.SelectedItem.ToString();
+
+            if (unidadTiempo == "Segundos")
+            {
+                tiempoSemaforo *= 1000;//1segundo - 1000 milisegundos
+            }
+            else
+            {
+                tiempoSemaforo *= 60 * 1000;//1 minuto - 60000 milisegundos
+            }
+
+            while (entrada.Count > 0 || salida.Count > 0)//se entra siempre que haya elementos en alguna de las colas entrada o salida
+            {
+                try
+                {
+                    //utilizo un SemaphoreSlim llamado semaforo para controlar el acceso al semáforo y sincronizar las operaciones. WaitAsync(token) espera de forma asíncrona hasta que se adquiere el semáforo. El token de cancelación se pasa como argumento para permitir la cancelación de la operación.
+                    await semaforo.WaitAsync(token);
+                    if (entrada.Count > 0)//si hay vehiculos en la cola de entrada
+                    {
+                        pictureBox1.BackColor = Color.Green;
+                        entrada.Dequeue();//Se desencola un vehículo de la cola de entrada con entrada.Dequeue().
+                        lblEntrada.Text = $"Coches Entrando: {entrada.Count}";//muestro la cantidad de vehículos restantes en la cola de entrada.
+                        await System.Threading.Tasks.Task.Delay(tiempoSemaforo, token);//Se espera de forma asíncrona durante el tiempo especificado en tiempoSemaforo antes de continuar.
+                    }
+
+                    pictureBox1.BackColor = Color.Red;
+                    await System.Threading.Tasks.Task.Delay(tiempoSemaforo, token);
+
+                    if (salida.Count > 0)
+                    {
+                        pictureBox1.BackColor = Color.Green;
+                        salida.Dequeue();
+                        lblSalida.Text = $"Coches Saliendo: {salida.Count}";
+                        await System.Threading.Tasks.Task.Delay(tiempoSemaforo, token);
+                    }
+
+                    pictureBox1.BackColor = Color.Red;
+                }
+                catch (OperationCanceledException) { }
+                finally
+                {
+                    semaforo.Release();//se libera el semaforo para permitir que otras operaciones lo adquieran.
+                }
+            }
+        }
+    }
+}
